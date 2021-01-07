224ce2bb07e09fd4a23348a1cf85766bc9278e0f
Beyzanur Kök <beyznrkk.55@gmail.com>
2021-01-07 22:16:18 +0300

    added Challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float spawnInterval = 1.0f;
+    private float cooldown = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown<=0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = spawnInterval;
+        }
+    }
+}
