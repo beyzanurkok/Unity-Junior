b124b615e03d46331281c528f6b5d74ac59fad50
Beyzanur Kök <beyznrkk.55@gmail.com>
2021-01-09 01:34:56 +0300

    added Personel Project

---
+++ PersonalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float ztopBoundary=19.0f;
+    private float zbottomBoundary = 2.6f;
+    private float speed = 10.0f;
+
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Moves the player based on arrow key input
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    //Prevent the player from leaving top or bottom of the screen
+
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z > ztopBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ztopBoundary);
+        }
+        if (transform.position.z < zbottomBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zbottomBoundary);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collision with enemy");
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+;
+        }
+    }
+}
---
+++ PersonalProject/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject powerupPrefab;
+    private float zEnemySpawn = 20.0f;
+    private float xSpawnRange=14.0f;
+    private float zPowerupSpawnRange=14.0f;
+    private float startDelay = 1.0f;
+    private float enemySpawnTime = 1.0f;
+    private float powerupSpawnTime = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, enemySpawnTime);
+        InvokeRepeating("SpawnRandomPowerup", startDelay, powerupSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    private void SpawnRandomEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, 0.75f, zEnemySpawn);
+
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+    }
+    private void SpawnRandomPowerup()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        float randomZ = Random.Range(-zPowerupSpawnRange, zPowerupSpawnRange);
+
+        Vector3 spawnPos = new Vector3(randomX,0.75f,randomZ);
+
+        Instantiate(powerupPrefab, spawnPos, powerupPrefab.transform.rotation);
+    }
+}
