e8c317f7c02c268d82b1f536b7393407b7281d2b
Beyzanur Kök <beyznrkk.55@gmail.com>
2021-01-08 23:55:49 +0300

    added ProtoType 3

---
+++ ProtoType 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(35, 0, 0);
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("spawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnObstacle()
+    {
+        if (playerControllerScript.isGameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        }
+        
+    }
+}
