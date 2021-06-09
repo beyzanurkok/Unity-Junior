2f59bfd274015f899d984d43a2341aff2ed2c334
Beyzanur Kök <beyznrkk.55@gmail.com>
2021-06-09 16:40:22 +0300

    VR 8 and VR-11

---
+++ VR11/Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    public Animator animController { get { return GetComponent<Animator>(); } }
+    private GameObject room;
+    Material[] materials;
+    public Material wallMaterial;
+    void Start()
+    {
+        room = GameObject.Find("room");
+        materials = room.GetComponent<MeshRenderer>().sharedMaterials;
+
+    }
+
+    // Update is called once per frame
+    public void SetMaterial()
+    {
+        animController.SetTrigger("click");
+        materials[0] = wallMaterial;
+        
+        //room.GetComponent<MeshRenderer>().sharedMaterials[0] = materials[0];
+        room.GetComponent<MeshRenderer>().sharedMaterials = materials;
+
+
+    }
+}
